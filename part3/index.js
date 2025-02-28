eb0d11131dc2d2218aa649bb69fbc69c9d3322b0
brunoc88 <brunocerutti88@gmail.com>
2025-02-27 22:27:28 -0300

    Update index.js

--- part3/index.js
+++ part3/index.js
@@ -122,8 +122,7 @@ app.post('/api/persons', (req, res) => {
 
 
 
-const PORT = 3001;
-
-app.listen(PORT,()=>{
-    console.log(`Listen Port: ${PORT}`);
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
